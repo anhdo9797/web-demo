e6dd424b768b13705b95e554701615310dc83c5c
anhdo9797 <docongphuocanh.2697@gmail.com>
2020-08-22 10:12:46 +0700

    Updates

---
+++ precache-manifest.fb075a09e8e2a2b9a0c8f5007c2e4913.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4614b6a34290ab1549f4f69a2bef4387",
+    "url": "/web-demo/index.html"
+  },
+  {
+    "revision": "7ab07211d52c84d0933e",
+    "url": "/web-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8745f9e976ea86177b55",
+    "url": "/web-demo/static/js/2.3b220889.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-demo/static/js/2.3b220889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab07211d52c84d0933e",
+    "url": "/web-demo/static/js/main.cba60386.chunk.js"
+  },
+  {
+    "revision": "62b3cedb4e2f727d9bb2",
+    "url": "/web-demo/static/js/runtime-main.84838078.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
